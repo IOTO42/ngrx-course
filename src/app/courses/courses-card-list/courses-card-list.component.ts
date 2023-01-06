2abaaf1004c96774515567b0d5d97672ef09f606
Areus-L <109914732+IOTO42@users.noreply.github.com>
2023-01-06 17:21:31 +0100

    course done :)

--- src/app/courses/courses-card-list/courses-card-list.component.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.ts
@@ -1,13 +1,15 @@
-import {Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation} from '@angular/core';
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation} from '@angular/core';
 import {Course} from "../model/course";
 import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
 import {EditCourseDialogComponent} from "../edit-course-dialog/edit-course-dialog.component";
 import {defaultDialogConfig} from '../shared/default-dialog-config';
+import { CourseEntityService } from '../services/course-entity.service';
 
 @Component({
     selector: 'courses-card-list',
     templateUrl: './courses-card-list.component.html',
-    styleUrls: ['./courses-card-list.component.css']
+    styleUrls: ['./courses-card-list.component.css'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CoursesCardListComponent implements OnInit {
 
@@ -18,7 +20,9 @@ export class CoursesCardListComponent implements OnInit {
     courseChanged = new EventEmitter();
 
     constructor(
-      private dialog: MatDialog ) {
+      private dialog: MatDialog,
+      private coursesService: CourseEntityService
+      ) {
     }
 
     ngOnInit() {
@@ -42,7 +46,9 @@ export class CoursesCardListComponent implements OnInit {
     }
 
   onDeleteCourse(course:Course) {
-
+    this.coursesService.delete(course).subscribe(
+      () => console.log('Delete completed'),
+      err => console.log('Delete failed', err));
 
   }
 
